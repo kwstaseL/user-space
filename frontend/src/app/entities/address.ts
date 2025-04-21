c08748d88e05ac013aa13c95b51a061ad628aeb8
kwstaseL <kostasel19@gmail.com>
2025-04-21 15:31:46 +0300

    Minor tweaks in display users page

--- frontend/src/app/entities/address.ts
+++ frontend/src/app/entities/address.ts
@@ -1,7 +1,6 @@
 import { AddressType } from '../utils/enums';
 
 export interface Address {
-  id: number;
   addressType: AddressType;
   addressText: string;
 }
--- frontend/src/app/entities/user.ts
+++ frontend/src/app/entities/user.ts
@@ -2,11 +2,11 @@ import { Gender } from '../utils/enums';
 import { Address } from './address';
 
 export interface User {
-  id: number;
+  id?: number;
   name: string;
   surname: string;
   gender: Gender;
   birthDate: string;
   addresses?: Address[];
-  age: number;
+  age?: number;
 }
